415ea55cab6e07a45b6533cabe07ee9795ff2ceb
Madnass92 <yesowrk@gmail.com>
2023-08-29 19:47:19 -0600

    confirmar

---
+++ index.js
@@ -0,0 +1,4 @@
+const displayvaloranterior = document.getElementById('valor-anterior');
+const displayvaloractual = document.getElementById('valor-actual');
+const botonesNumero = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
