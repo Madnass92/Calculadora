b23433244b492c5495fe771118a2b5d8661a38b1
Madnass92 <yesowrk@gmail.com>
2023-08-29 22:48:12 -0600

    commit inicial

---
+++ Display.js
@@ -0,0 +1,8 @@
+class Display {
+    constructor(displayValorAnterior, displayValorActual){
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculador = new Calculadora();
+        this.displayValorActual
+    }
+}
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const displayvaloranterior = document.getElementById('valor-anterior');
-const displayvaloractual = document.getElementById('valor-actual');
-const botonesNumero = document.querySelectorAll('.numero');
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
+
+const calculadora = new Calculadora();
+
+console.log(calculadora.sumar(2,3))
+console.log(calculadora.resta(2,3))
+console.log(calculadora.multiplicar(2,3))
+console.log(calculadora.dividir(2,3))
